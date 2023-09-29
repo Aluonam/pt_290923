eecb31a8bcc1eed17f0337adf75fe5ef540a28b4
Paula Cano <paulacanoroman@gmail.com>
2023-09-29 21:18:20 +0200

    consumo API

--- pt-290923/components/SearchPokemon.jsx
+++ pt-290923/components/SearchPokemon.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const SearchPokemon = () => {
 
@@ -6,11 +6,20 @@ const SearchPokemon = () => {
     
     const [dataInput, setDataInput] = useState("")
 
-
+    useEffect(() => {
+      apiCall()
+    }, [])
+    
+    const apiCall = async () =>{
+        const call = await fetch(`https://pokeapi.co/api/v2/pokemon/ditto`);
+        const data = await call.json();;
+        console.log(data)
+        setDataAPI(data)
+    }
 
   return (
     <>
-    <input onChange={(e)=>{console.log(e.target.value)}}></input>
+    <input onChange={(e)=>{setDataInput(e.target.value)}} value={dataInput}></input>
     </>
   )
 }
